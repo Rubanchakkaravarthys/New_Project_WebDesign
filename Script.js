a3ed6114e3f6b3c7315b5debc57df9c09526fbb3
Rubanchakkaravarthys <160729723+Rubanchakkaravarthys@users.noreply.github.com>
2024-09-27 19:02:47 +0530

    some changes

--- Script.js
+++ Script.js
@@ -9,20 +9,5 @@ document.getElementById('cancelBtn').addEventListener('click', function() {
                 messageDiv.style.display = 'none'; 
             });
 
-// Address
-document.getElementById('addAddress').addEventListener('click',function(){
-    let addressCount=3;
-    addressCount++;
-    const addressContainer = document.getElementById('addressContainer');
-    const newAddressSection = document.createElement('div');
-    newAddressSection.className='col address-section';
-    newAddressSection.innerHTML=
-    `
-     <h5 class="text-white"><span><input type="checkbox"></span> Address ${addressCount}</h5>
-                <input type="text" placeholder="Platform"><br>
-                <input type="text" placeholder="Address" ><br>
-                <input type="text" placeholder="Pincode"><br>
-    `;
-    addressContainer.appendChild(newAddressSection);
-})
+
 
